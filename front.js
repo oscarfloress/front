89371992d5fd003fe8b5c4cd8a274156cc8adc17
oscarfloress <oscar.flores@alumno.udg.mx>
2021-05-17 16:10:05 -0500

    Add files via upload

---
+++ front.js
@@ -0,0 +1,12 @@
+function msg() {
+    alert("Hello Javatpoint");
+}
+
+function buscarProducto() {
+    var buscarProducto = document.getElementById("buscarProducto");
+    alert("Valor de la busqueda es: " + buscarProducto.value);
+}
+
+function buscarPorFabricante() {
+    alert("Hello Javatpoint");
+}
