2be4bb7bd1b5fc5689d3a3b1629c8772c3f29fa8
Oscar Flores <oscar.flores@alumno.udg.mx>
2021-05-17 17:56:57 -0500

    filtrar select

--- js/front.js
+++ js/front.js
@@ -1,12 +1,27 @@
-function msg() {
-    alert("Hello Javatpoint");
-}
+
+$(document).ready(function () {
+
+    $('select').on('change', function (e) {
+        var elements = $('div.fabricante');
+
+        elements.each(function (index, domElement) {
+            var $element = $(domElement);
+
+            var valueSelected = $('select').find('option:selected').val();
+
+            var cadena = $element.text();
+            if (cadena.includes(valueSelected)) 
+                $element.parent().parent().show();
+                
+            else $element.parent().parent().hide();
+        });
+    });
+
+});
 
 function buscarProducto() {
     var buscarProducto = document.getElementById("buscarProducto");
     alert("Valor de la busqueda es: " + buscarProducto.value);
 }
 
-function buscarPorFabricante() {
-    alert("Hello Javatpoint");
-}
+
